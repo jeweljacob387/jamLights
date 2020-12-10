e32caf8f7b17274354ab0784fa5df88b331ce955
jeweljacob387 <jeweljacob387@gmail.com>
2020-12-11 01:33:51 +0530

    read from json initialised

--- hardware/frames.py
+++ hardware/frames.py
@@ -1,4 +1,5 @@
 import nodes
+from json import load
 
 
 class Frame:
@@ -13,24 +14,18 @@ frames = [
     Frame([nodes.tree2, nodes.tree3], 5000),
     Frame([nodes.tree3], 5400),
     Frame([nodes.tree1, nodes.streetLight3, nodes.tree3], 3000),
-    Frame([nodes.tree1, nodes.tree2], 1000),
-    Frame([nodes.tree2], 2000),
-    Frame([nodes.tree2, nodes.tree3], 5000),
-    Frame([nodes.tree3], 5400),
-    Frame([nodes.tree1, nodes.tree2, nodes.tree3], 3000),
-    Frame([nodes.streetLight1, nodes.tree2], 1000),
-    Frame([nodes.tree2], 2000),
-    Frame([nodes.tree2, nodes.tree3], 5000),
-    Frame([nodes.tree3], 5400),
-    Frame([nodes.tree1, nodes.star3, nodes.tree3], 3000),
-    Frame([nodes.tree1, nodes.tree2], 1000),
-    Frame([nodes.tree2], 2000),
-    Frame([nodes.tree2, nodes.tree3], 5000),
-    Frame([nodes.tree3], 5400),
-    Frame([nodes.tree1, nodes.tree2, nodes.tree3], 3000),
-    Frame([nodes.tree1, nodes.tree2], 1000),
-    Frame([nodes.tree2], 2000),
-    Frame([nodes.tree2, nodes.star1], 5000),
-    Frame([nodes.tree3], 5400),
-    Frame([nodes.tree1, nodes.tree2, nodes.tree3], 3000)
+    Frame([nodes.tree1, nodes.tree2], 1000)
 ]
+
+with open('../framesJons/frames.json') as f:
+    data = load(f)
+
+frames = data['frames']
+duration = len(frames[0]['nodes'][0]['frameSates'])
+print(duration)
+for i in range(duration):
+    for frame in frames:
+        for node in frame['nodes']:
+            for time in node['frameSates']:
+                if time:
+                    print(time)
