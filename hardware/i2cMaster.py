f02bca85dd0341b428e6979a52a3ab17005201ed
jeweljacob387 <jeweljacob387@gmail.com>
2020-12-11 23:59:42 +0530

    print disabled audio file changed

--- hardware/i2cMaster.py
+++ hardware/i2cMaster.py
@@ -8,21 +8,23 @@ startTime = time()
 
 
 def setFrame(frame: Frame):
-    os.system('clear')
+    # os.system('clear')
     for node in nodes:
         if node.name in [frameNode.name for frameNode in frame.nodes]:
             turnOnNode(node)
         else:
             turnOffNode(node)
-    print('\033[37m', "...")
+    # print('\033[37m', "...")
     sleep(40/1000)
 
 
 def turnOnNode(node):
-    print('\033[32m', "Turn on group", node.group, "'s device", node.deviceIndex,
-          "at", time() - startTime)
+    # print('\033[32m', "Turn on group", node.group, "'s device", node.deviceIndex,
+    #       "at", time() - startTime)
+    return
 
 
 def turnOffNode(node):
-    print('\033[31m', "Turn off group", node.group, "'s device", node.deviceIndex,
-          "at",  time() - startTime)
+    # print('\033[31m', "Turn off group", node.group, "'s device", node.deviceIndex,
+    #       "at",  time() - startTime)
+    return
